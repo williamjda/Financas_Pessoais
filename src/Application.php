6f5454cda6cd841239e6d4379a7420b52c2bed29
William Aquino <william.aquino@atitude.com.br>
2018-02-26 19:04:35 -0300

    Escrevendo o Application

---
+++ src/Application.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: William.Aquino
+ * Date: 26/02/2018
+ * Time: 13:32
+ */
+
+namespace SONFin;
+
+
+class Application
+{
+    private $serviceContainer;
+
+    public function service($name);
+}
