e194a64bd27c130a01f336bc4d3cf2676d5f16bb
William Aquino <william.aquino@atitude.com.br>
2018-02-27 11:55:31 -0300

    Ajuste do composer.lock

--- src/Application.php
+++ src/Application.php
@@ -1,17 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: William.Aquino
- * Date: 26/02/2018
- * Time: 13:32
- */
 
+
+declare(strict_types=1);
 namespace SONFin;
 
+use SONFin\Plugins\PluginInterface;
 
 class Application
 {
     private $serviceContainer;
 
-    public function service($name);
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service($name){
+        return $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service): void
+    {
+        if(is_callable($service)){
+            $this->serviceContainer->addLazy($name, $service);
+        }else{
+            $this->serviceContainer->add($name, $service);
+        }
+    }
+
+    public function plugin(PluginInterface $plugin): void
+    {
+        $plugin->register($this->serviceContainer);
+    }
+
 }
